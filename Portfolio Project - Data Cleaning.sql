afe39f42eed104bc77387a75d08f875bb51b6ac3
azadehhme80 <76241456+azadehhme80@users.noreply.github.com>
2024-05-15 15:23:24 -0400

    Update Portfolio Project - Data Cleaning.sql
    v2

--- Portfolio Project - Data Cleaning.sql
+++ Portfolio Project - Data Cleaning.sql
@@ -12,7 +12,9 @@ FROM world_layoffs.layoffs;
 
 
 
--- first thing we want to do is create a staging table. This is the one we will work in and clean the data. We want a table with the raw data in case something happens
+-- first thing we want to do is create a staging table. This is the one we will work in and clean the data.
+-- We want a table with the raw data in case something happens
+
 CREATE TABLE world_layoffs.layoffs_staging 
 LIKE world_layoffs.layoffs;
 
